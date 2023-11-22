1491effbb700b4bb375cd110d3ab48890f54643a
Danzarote <luishdaniel@gmail.com>
2023-11-21 22:23:30 -0300

    Proyecto Casi Listo

---
+++ clinica-odontologica/src/main/java/com/backend/clinicaodontologica/service/impl/OdontologoService.java
@@ -0,0 +1,106 @@
+package com.backend.clinicaodontologica.service.impl;
+
+import com.backend.clinicaodontologica.dto.entrada.odontologo.OdontologoEntradaDto;
+import com.backend.clinicaodontologica.dto.modificacion.OdontologoModificacionEntradaDto;
+import com.backend.clinicaodontologica.dto.salida.odontologo.OdontologoSalidaDto;
+import com.backend.clinicaodontologica.entity.Odontologo;
+import com.backend.clinicaodontologica.repository.OdontologoRepository;
+import com.backend.clinicaodontologica.service.IOdontologoService;
+import com.backend.clinicaodontologica.utils.JsonPrinter;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+public class OdontologoService implements IOdontologoService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(OdontologoService.class);
+
+    private OdontologoRepository odontologoRepository;
+
+    private ModelMapper modelMapper;
+
+    @Autowired //no es necesario ponerlo
+    public  OdontologoService(OdontologoRepository odontologoRepository, ModelMapper modelMapper) {
+        this.odontologoRepository = odontologoRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public OdontologoSalidaDto registrarOdontologo(OdontologoEntradaDto odontologo) {
+        //convertimos mediante el mapper de dtoEntrada a entidad
+        LOGGER.info("OdotologoEntradaDto: " + JsonPrinter.toString(odontologo));
+        Odontologo odontologoEntidad = modelMapper.map(odontologo, Odontologo.class);
+
+        //mandamos a persistir a la capa dao y obtenemos una entidad
+        Odontologo odontologoAPersistir = odontologoRepository.save(odontologoEntidad);
+        //transformamos la entidad obtenida en salidaDto
+        OdontologoSalidaDto odontologoSalidaDto = modelMapper.map(odontologoAPersistir, OdontologoSalidaDto.class);
+        LOGGER.info("OdontologoSalidaDto: " + JsonPrinter.toString(odontologoSalidaDto));
+        return odontologoSalidaDto;
+    }
+
+    public List<OdontologoSalidaDto> listarOdontologos() {
+        List<OdontologoSalidaDto> odontologosSalidaDto = odontologoRepository.findAll()
+                .stream()
+                .map(odontologo -> modelMapper.map(odontologo, OdontologoSalidaDto.class))
+                .toList();
+
+
+        if (LOGGER.isInfoEnabled())
+            LOGGER.info("Listado de todos los pacientes: {}", JsonPrinter.toString(odontologosSalidaDto));
+        return odontologosSalidaDto;
+    }
+
+    @Override
+    public OdontologoSalidaDto buscarOdontologoPorId(Long id) {
+        Odontologo odontologoBuscado = odontologoRepository.findById(id).orElse(null);
+        OdontologoSalidaDto odontologoEncontrado = null;
+
+        if (odontologoBuscado != null) {
+            odontologoEncontrado = modelMapper.map(odontologoBuscado, OdontologoSalidaDto.class);
+            LOGGER.info("Odontologo encontrado: {}", JsonPrinter.toString(odontologoEncontrado));
+        } else LOGGER.error("El id no se encuentra registrado en la base de datos");
+
+        return odontologoEncontrado;
+    }
+
+    @Override
+    public OdontologoSalidaDto actualizarOdontologo(OdontologoModificacionEntradaDto odontologo) {
+        Odontologo odontologoRecibido = modelMapper.map(odontologo, Odontologo.class);
+        Odontologo odontologoAActualizar = odontologoRepository.findById(odontologoRecibido.getId()).orElse(null);
+
+        OdontologoSalidaDto odontologoSalidaDto = null;
+
+        if (odontologoAActualizar != null) {
+            odontologoAActualizar = odontologoRecibido;
+            odontologoRepository.save(odontologoAActualizar);
+
+            odontologoSalidaDto = modelMapper.map(odontologoAActualizar, OdontologoSalidaDto.class);
+            LOGGER.warn("Odontologo actualizado: {}", JsonPrinter.toString(odontologoSalidaDto));
+
+        } else {
+            LOGGER.error("No fue posible actualizar el odontologo porque no se encuentra en nuestra base de datos");
+            //lanzar excepcion correspondiente
+        }
+
+
+        return odontologoSalidaDto;
+    }
+
+    @Override
+    public void eliminarOdontologo(Long id) {
+        if (odontologoRepository.findById(id).orElse(null) != null) {
+            odontologoRepository.deleteById(id);
+            LOGGER.warn("Se ha eliminado el odontologo con id: {}", id);
+        } else {
+            LOGGER.error("No se ha encontrado el odontologo con id {}", id);
+            //excepcion a lanzar aqui
+        }
+
+    }
+
+
+}
