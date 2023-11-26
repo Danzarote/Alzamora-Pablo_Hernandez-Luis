f00b03ea12b69ad0ffe0e0a0a48b720675a427f7
Luis <luishdaniel@gmail.com>
2023-11-26 11:47:58 -0300

    Arreglago metodo buscar por id de Odontologo

--- clinica-odontologica/src/main/java/com/backend/clinicaodontologica/repository/OdontologoRepository.java
+++ clinica-odontologica/src/main/java/com/backend/clinicaodontologica/repository/OdontologoRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OdontologoRepository extends JpaRepository<Odontologo, Long> {
 
     // Método para buscar odontólogos por matrícula
-    Odontologo findById(int id);
+    Optional<Odontologo> findById(Long id);
 
     // Método para buscar odontólogos por apellido
     List<Odontologo> findByApellido(String apellido);
